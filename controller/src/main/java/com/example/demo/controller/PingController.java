ba36a338488126b7b3d467bf8f30aa5b2dfe6feb
Maxence Petitpierre <maxence.petitpierre@vadesecure.com>
2021-09-22 23:13:59 +0200

    Test: test controllers

--- controller/src/main/java/com/example/demo/controller/PingController.java
+++ controller/src/main/java/com/example/demo/controller/PingController.java
@@ -2,10 +2,16 @@ package com.example.demo.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingController {
+
+    @GetMapping(path = "/")
+    public ResponseEntity<String> getHome() {
+        return ResponseEntity.ok("");
+    }
 
     @GetMapping(path = "/ping")
     public ResponseEntity<String> getPing() {
--- controller/src/main/java/com/example/demo/test/StockControllerTest.java
+++ controller/src/main/java/com/example/demo/test/StockControllerTest.java
@@ -4,14 +4,23 @@ package com.example.demo.test;
 import com.example.demo.controller.StockController;
 import com.example.demo.dto.in.ShoeFilter;
 import com.example.jpa.ShoeRepository;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import javax.servlet.ServletContext;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,11 +30,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@WebMvcTest(StockController.class)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = { StockController.class })
+@WebAppConfiguration
 public class StockControllerTest {
 
     @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
     private MockMvc mockMvc;
+    @BeforeEach
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    public void givenWac_whenServletContext_thenItProvidesGreetController() {
+        ServletContext servletContext = webApplicationContext.getServletContext();
+        Assert.assertNotNull(servletContext);
+        Assert.assertTrue(servletContext instanceof MockServletContext);
+        Assert.assertNotNull(webApplicationContext.getBean(""));
+    }
 
     @Mock
     private ShoeRepository mockShoeRepository;
@@ -39,7 +65,6 @@ public class StockControllerTest {
     public void getSockTest() throws Exception {
         List<ShoeFilter> shoeFilters = new ArrayList();
         when(mockShoeRepository.findAll()).thenReturn(shoeFilters);
-
         this.mockMvc.perform(get("/stock")).andDo(print()).andExpect(status().isOk());
     }
 }
